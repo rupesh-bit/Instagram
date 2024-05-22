35369ef3b70e91db1ba0a46046482fa793c6ff38
rupesh <you@example.com>
2024-05-23 00:09:27 +0530

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + Rupesh kumar singh</h1>
-      <div className="card">
+      <div className="card ">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
