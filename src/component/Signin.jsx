201ac2a21cbeec6817f23a60cecf5af147961468
rupesh <you@example.com>
2024-07-10 13:38:19 +0530

    gest login

--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -33,16 +33,27 @@ useEffect(()=>{
 
 
 
- const Login= async(da)=> {
-    
-    
-     const se=await  authService.login(da);
+ const Login= async(data)=> {
+     const se=await  authService.login(data);
      if(se){ 
      const userData= await authService.getCurrentUser()
           dispatch(login({userData}));
           navigate('/')
         }
   }
+
+  const Gest= async(data)=> {
+    data={email: 'three@gmail.com', password: '12345678'}
+ console.log(data)
+  const se=await  authService.login(data);
+  if(se){ 
+  const userData= await authService.getCurrentUser()
+       dispatch(login({userData}));
+       navigate('/')
+     }
+}
+
+
 
 if(!loding){return (
  <div className="min-h-screen bg-gray-950 flex justify-center items-center flex-col text-white">
@@ -80,7 +91,7 @@ return (
 
  <div className="lg:w-1/2 w-full h-full bg-zinc-800 lg:ml-[1px] flex items-center justify-center ">
 
-  <div className="sm:w-4/5 w-full border-orange-600 border-2 bg-zinc-400 lg:h-1/2 rounded-lg flex items-center justify-center"> 
+  <div className="sm:w-4/5 w-full border-orange-600 border-2 bg-zinc-400 lg:h-2/3 rounded-lg flex items-center justify-center"> 
 
     <div className='w-2/3 justify-center flex items-center flex-col '>
 
@@ -99,26 +110,34 @@ return (
               }})}>
            <div className=" border-zinc-700 py-2 lg:py-3 px-5  border-[1px] rounded-full mb-1"> 
               <label className='ml-4' htmlFor='email'>Email:</label>
-             <input type="email" id='email' placeholder="Email" {...register("email",{required:true})} />
+             <input type="email" id='email' placeholder="Email" {...register("email")} />
           </div> 
           <div className="mb-1 border-zinc-700 py-2 lg:py-3 px-5  border-[1px] rounded-full"> 
               <label className='mb-3'htmlFor='password'>password:</label>
-               <input id='password' type="password" placeholder="Password"  {...register("password",{required:true})} />
+               <input id='password' type="password" placeholder="Password"  {...register("password")} />
            </div>
           <div className="mb-6 border-zinc-700 py-2 lg:py-3 px-5  border-[1px] rounded-full">
                <label className='mb-3' htmlFor='name'>name:</label>
-              <input id='name' type="text" placeholder="Name"  {...register("name",{required:true})}/> 
+              <input id='name' type="text" placeholder="Name"  {...register("name")}/> 
            </div>
-
-           <div className='w-full flex justify-center sm:inline'><button className=' border-orange-600 border-2 rounded-full px-5 py-4  sm:mx-6 m-2 text-xl' 
+          <div className=''>
+           <div className='w-full  flex justify-center sm:inline'><button className=' border-orange-600 border-2 rounded-full px-5 py-4  sm:mx-6 m-2 text-xl' 
            type="button" onClick={handleSubmit(Login)}>
-             login
+             Login
+
+           </button></div>
+           <div className='w-full  flex justify-center sm:inline'><button className='mx-auto border-orange-600 border-2 rounded-full px-5 py-4 text-xl'
+                        type="submit">
+                         Register
+                       </button></div>
+          </div>
+
+           <div className='w-full  flex justify-center sm:inline ml-6'><button className=' border-orange-600 border-2 rounded-full px-5 py-4  sm:mx-6 m-2 text-xl' 
+             type="button" onClick={handleSubmit(Gest)}>
+                Login as a Gest
            </button></div>
 
-           <div className='w-full flex justify-center sm:inline'><button className='mx-auto border-orange-600 border-2 rounded-full px-5 py-4 text-xl'
-             type="submit">
-             Register
-           </button></div>
+           
       </form>
     </div>
   </div>
@@ -130,5 +149,3 @@ return (
 
 }
 
-// await authService.createAccount({...da});
-   //             login(da)
